c643e414ac4be6eb5957a9e7a571fbfb35295a7a
en <ehsiaoz@h2cap.com>
2017-01-27 13:15:17 -0600

    JAWS DB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-    host     : '127.0.0.1',
-    user     : 'root',
-    password : 'root',
-    database : 'burger_db',
-    port     : '3306',
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+      host     : '127.0.0.1',
+      user     : 'root',
+      password : 'root',
+      database : 'burger_db',
+      port     : '3306',
+  });
+};
 
 //Establish connection to mySQL DB
 connection.connect(function(err) {
